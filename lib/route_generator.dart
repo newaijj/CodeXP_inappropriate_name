36cb6c976b1941259e4ae9af6bc2f971fbebded6
Yue Hern <yuehernkan@gmail.com>
2020-06-01 12:51:00 +0800

    Clean up

---
+++ lib/route_generator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  final BuildContext context;
+
+  AppRouter(this.context);
+  Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+    switch (settings.name) {
+
+    }
+  }
+}
